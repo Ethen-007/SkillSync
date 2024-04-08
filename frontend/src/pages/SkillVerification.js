1895a8f6b7f63c46d1864f516b5b8bf8672f7ec7
Aaron Thomas Blessen <85331681+Aaron-Thomas-Blessen@users.noreply.github.com>
2024-04-08 20:56:43 +0530

    Update SkillVerification.js

--- frontend/src/pages/SkillVerification.js
+++ frontend/src/pages/SkillVerification.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import html from "../images/html.png"; // Import images for each skill
+import html from "../images/html.png";
 import css from "../images/css.png";
 import java from "../images/javascript.jpg";
 import react from "../images/react.jpg";
@@ -9,9 +9,8 @@ import python from "../images/python.png";
 
 const SkillVerification = () => {
   const location = useLocation();
-  const skillName = location.pathname.split("/").pop(); // Extract the skill name from the URL
+  const skillName = location.pathname.split("/").pop(); 
 
-  // Map skill names to their corresponding imported images
   const skillImages = {
     html: html,
     css: css,
@@ -21,7 +20,6 @@ const SkillVerification = () => {
     python: python,
   };
 
-  // Get the image corresponding to the skill name
   const imageUrl = skillImages[skillName];
 
   return (
